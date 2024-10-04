cbff6643ed2a25aac47916a91903e939665fc094
ashrafulislam65 <md.ashraful.islam7758a@gmail.com>
2024-10-05 04:52:00 +0600

    added posted time

--- js/video.js
+++ js/video.js
@@ -1,5 +1,12 @@
 console.log('videos')
 //get
+function getTimeString(time){
+    const hour = parseInt(time/3600);
+    let remainingSecond = time % 3600;
+    const minute = parseInt(remainingSecond/60);
+    remainingSecond = remainingSecond % 60;
+    return `${hour} hour ${minute} minute  ${remainingSecond} second ago`
+}
 const loadCatogories = () => {
     fetch("https://openapi.programming-hero.com/api/phero-tube/categories")
         .then((res) => res.json())
@@ -51,11 +58,13 @@ const displayVideos = (videos) => {
         card.classList = "card card-compact "
         card.innerHTML =
             `
-        <figure class="h-[200px]">
+        <figure class="h-[200px] relative">
           <img
           src=${video.thumbnail}
           class="h-full w-full object-cover"
           alt="Shoes" />
+          ${video.others.posted_date?.length ==0 ?"" : `<span class="absolute right-2 bottom-2 bg-black rounded p-1 text-white">${getTimeString(video.others.posted_date)}</span>`}
+          
         </figure>
         <div class="px-0 py-2 flex gap-2">
            <div>
@@ -65,7 +74,7 @@ const displayVideos = (videos) => {
            <h2 class="font-bold">${video.title}</h2>
            <div class="flex items-center gap-2">
             <p class="text-gray-400">${video.authors[0].profile_name}</p>
-            <img class="w-5" src="https://img.icons8.com/?size=48&id=98A4yZTt9abw&format=png" />
+            ${video.authors[0].verified === true ?`<img class="w-5" src="https://img.icons8.com/?size=48&id=98A4yZTt9abw&format=png" />` : ''}
            </div>
            
            <p></p>
